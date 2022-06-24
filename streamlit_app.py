4d48492d6750018fa40766644b6aa63c6b766fa0
stepmoreira <88782811+stepmoreira@users.noreply.github.com>
2022-06-24 14:21:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,5 @@ streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
+# write your own comment - creates a table
+streamlit.dataframe(fruityvice_normalized)
